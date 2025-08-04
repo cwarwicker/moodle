732fa066aaf503c2a447268ca21f9fdab9a77bb5
Conn Warwicker <conn.warwicker@catalyst-eu.net>
2025-08-04 09:30:45 +0100

    fix some phpunit issues

--- mod/assign/tests/externallib_test.php
+++ mod/assign/tests/externallib_test.php
@@ -735,7 +735,6 @@ final class externallib_test extends \mod_assign\externallib_advanced_testcase {
         $userflag->mailed = 0;
         $userflag->extensionduedate = 0;
         $userflag->workflowstate = 'inmarking';
-        $userflag->allocatedmarker = $USER->id;
 
         $DB->insert_record('assign_user_flags', $userflag);
 
@@ -757,7 +756,6 @@ final class externallib_test extends \mod_assign\externallib_advanced_testcase {
         $this->assertEquals(0, $userflag['mailed']);
         $this->assertEquals(0, $userflag['extensionduedate']);
         $this->assertEquals('inmarking', $userflag['workflowstate']);
-        $this->assertEquals($USER->id, $userflag['allocatedmarker']);
     }
 
     /**
@@ -2819,7 +2817,7 @@ final class externallib_test extends \mod_assign\externallib_advanced_testcase {
         $DB->update_record('user', $student);
 
         $this->setUser($teacher);
-        $participants = mod_assign_external::list_participants($assignment->id, 0, '', 0, 0, false, true, true);
+        $participants = mod_assign_external::list_participants($assignment->id, 0, '', 0, 0, false, true, true, false);
         $participants = external_api::clean_returnvalue(mod_assign_external::list_participants_returns(), $participants);
         $this->assertCount(1, $participants);
 
@@ -2838,7 +2836,7 @@ final class externallib_test extends \mod_assign\externallib_advanced_testcase {
         $this->assertEquals('', $participant['submissionstatus']);
         $this->assertArrayHasKey('enrolledcourses', $participant);
 
-        $participants = mod_assign_external::list_participants($assignment->id, 0, '', 0, 0, false, false, true);
+        $participants = mod_assign_external::list_participants($assignment->id, 0, '', 0, 0, false, false, true, false);
         $participants = external_api::clean_returnvalue(mod_assign_external::list_participants_returns(), $participants);
         // Check that the list of courses the participant is enrolled is not returned.
         $participant = $participants[0];
@@ -2914,7 +2912,7 @@ final class externallib_test extends \mod_assign\externallib_advanced_testcase {
         $this->setUser($teacher);
 
         // Test mod_assign_external::list_participants.
-        $participants = mod_assign_external::list_participants($assignmodule->id, $group->id, '', 0, 0, false, true, true);
+        $participants = mod_assign_external::list_participants($assignmodule->id, $group->id, '', 0, 0, false, true, true, false);
         $participants = external_api::clean_returnvalue(mod_assign_external::list_participants_returns(), $participants);
         $this->assertEquals($group->id, $participants[0]['groupid']);
         $this->assertEquals(format_string($gname, true), $participants[0]['groupname']);
