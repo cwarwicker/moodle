89186c4affa757711b2f7b6602bf87c0de0ff13c
Conn Warwicker <conn.warwicker@catalyst-eu.net>
2026-01-21 11:36:01 +0000

    MDL-86007 mod_assign: Multiple marker functionality in assignfeedback_editpdf.

--- public/mod/assign/feedback/editpdf/db/upgrade.php
+++ public/mod/assign/feedback/editpdf/db/upgrade.php
@@ -28,6 +28,15 @@
  * @return bool
  */
 function xmldb_assignfeedback_editpdf_upgrade($oldversion) {
+    global $DB;
+    $dbman = $DB->get_manager();
+
+    // Automatically generated Moodle v4.2.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Automatically generated Moodle v4.3.0 release upgrade line.
+    // Put any upgrade step following this.
+
     // Automatically generated Moodle v4.4.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -39,6 +48,45 @@ function xmldb_assignfeedback_editpdf_upgrade($oldversion) {
 
     // Automatically generated Moodle v5.1.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2025111903) {
+        $table = new xmldb_table('assignfeedback_editpdf_cmnt');
+        $field = new xmldb_field('markid', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'gradeid');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        $table = new xmldb_table('assignfeedback_editpdf_annot');
+        $field = new xmldb_field('markid', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'pageno');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        $table = new xmldb_table('assignfeedback_editpdf_rot');
+        $field = new xmldb_field('markid', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'pageno');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // The index on the rotations table needs to include markid. So we have to drop existing then re-create.
+        $index = new xmldb_index('gradeid_pageno', XMLDB_INDEX_UNIQUE, ['gradeid', 'pageno']);
+        if ($dbman->index_exists($table, $index)) {
+            $dbman->drop_index($table, $index);
+        }
+
+        // Now create new one.
+        $index = new xmldb_index('gradeid_markid_pageno', XMLDB_INDEX_UNIQUE, ['gradeid', 'markid', 'pageno']);
+
+        // Conditionally launch add index gradeid_pageno.
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+
+        upgrade_plugin_savepoint(true, 2025111903, 'assignfeedback', 'editpdf');
+    }
 
     return true;
 }
