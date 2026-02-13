10dd9f211281dfeac9687b290d8e7ba1c057682a
Conn Warwicker <connwarwicker@hotmail.co.uk>
2026-02-13 17:04:11 +0000

    MDL-87944 core_task: Add queue option to adhoc task cli script.

--- admin/cli/adhoc_task.php
+++ admin/cli/adhoc_task.php
@@ -41,6 +41,9 @@ list($options, $unrecognized) = cli_get_params(
         'classname' => null,
         'taskslimit' => null,
         'failed' => false,
+        'queue' => false,
+        'data' => null,
+        'userid' => null,
     ], [
         'h' => 'help',
         'e' => 'execute',
@@ -49,6 +52,9 @@ list($options, $unrecognized) = cli_get_params(
         'f' => 'force',
         'c' => 'classname',
         'l' => 'taskslimit',
+        'q' => 'queue',
+        'd' => 'data',
+        'u' => 'userid',
     ]
 );
 
@@ -72,6 +78,9 @@ Options:
  -c, --classname           Run tasks with a certain classname (FQN)
  -l, --taskslimit=N        Run at most N tasks
      --failed              Run only tasks that failed, ie those with a fail delay
+ -q, --queue               Queue a new adhoc task of the given --classname
+ -d, --data                Custom data to be added to the newly queued task in JSON format (Only usable in conjunction with --queue)
+ -u, --userid              User ID to be assigned to the newly queued task (Only usable in conjunction with --queue)
 
 Run all queued tasks:
 \$sudo -u www-data /usr/bin/php admin/cli/adhoc_task.php --execute
@@ -88,6 +97,16 @@ Run a specific task with debugging:
 
 To profile a long running task:
 \$sudo -u www-data /usr/bin/php admin/cli/adhoc_task.php --taskslimit=1 --classname='\\some\\class\\name' --ignorelimits
+
+Queue a new task of a specific class.
+\$sudo -u www-data /usr/bin/php admin/cli/adhoc_task.php --classname='\\local_myplugin\\task\\my_task' --queue
+
+Queue a new task of a specific class, and set the user id.
+\$sudo -u www-data /usr/bin/php admin/cli/adhoc_task.php --classname='\\local_myplugin\\task\\my_task' --queue --userid=123
+
+Queue a new task of a specific class, and set the user id and supply custom data to the task.
+\$sudo -u www-data /usr/bin/php admin/cli/adhoc_task.php --classname='\\local_myplugin\\task\\my_task' \
+    --queue --userid=123 --data='{"foo":"bar"}'
 
 EOT;
 
@@ -152,6 +171,54 @@ if (!empty($options['id'])) {
 // Run all failed tasks.
 if (!empty($options['failed'])) {
     \core\cron::run_failed_adhoc_tasks($classname);
+    exit(0);
+}
+
+// Queue a new task.
+if (!empty($options['queue'])) {
+    // Classname must be specified to queue a new task.
+    if (is_null($classname)) {
+        cli_error('Classname (--classname) must be specified with --queue option.');
+    }
+
+    // Get the custom data and userid if they are specified.
+    $data = (!empty($options['data']) ? json_decode($options['data'], true) : null);
+    $userid = (!empty($options['userid']) ? $options['userid'] : null);
+
+    // Try to find the class and queue an instance if it exists.
+    try {
+        $task = new $classname();
+    } catch (\Error $e) {
+        cli_error("Adhoc task ({$classname}) not found.");
+    }
+
+    if ($data) {
+        $task->set_custom_data($data);
+    }
+    if ($userid) {
+        $task->set_userid($userid);
+    }
+
+    // Queue the task and get it's ID back. Or false if it's already queued.
+    $id = \core\task\manager::queue_adhoc_task($task, true);
+
+    // Was it actually created? Sometimes it will fail if an exact match already exists.
+    if ($id) {
+        mtrace("Adhoc task queued: {$classname} [{$id}]" .
+            (!empty($options['data']) ? " (Data: {$task->get_custom_data_as_string()})" : "") .
+            (!empty($options['userid']) ? " (User: {$options['userid']})" : ""));
+    } else {
+        $id = \core\task\manager::get_queued_adhoc_task_record($task)->id;
+        mtrace("Adhoc task not queued. Exact match already exists in queue [{$id}].");
+    }
+
+    // Do we want to execute it straight away?
+    if ($options['execute']) {
+        \core\cron::run_adhoc_task($id);
+        exit(0);
+    }
+
+    // If not, we can stop here and nothing else will be relevant alongside the queue option.
     exit(0);
 }
 
